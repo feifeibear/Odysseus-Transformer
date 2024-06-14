0a5ca8e0f70038b28050d7381d796d0575ae9925
root <fangjiarui123@gmail.com>
2024-06-14 02:29:57 +0000

    add mlp test for TP-SP

---
+++ tests/mlp_test.py
@@ -0,0 +1,53 @@
+from utils.globals import _set_global_memory_buffer
+import torch
+import torch.testing as testing
+from decoder.tensor_parallel import LlamaMLPTPSP
+from transformers import LlamaConfig
+import torch.distributed as dist
+import os
+from fairscale.nn.model_parallel.initialize import (
+    initialize_model_parallel,
+)
+
+
+def test_LlamaMLPTPSP():
+    dist.init_process_group(backend="nccl")
+    _rank = int(os.environ["RANK"])
+    _world_size = int(os.environ["WORLD_SIZE"])
+    initialize_model_parallel(_world_size)
+
+    dev = torch.device(f"cuda:{_rank}")
+    _set_global_memory_buffer(dev)
+
+    intermediate_size = 2048
+    hidden_size = 512
+    seqlen = 32
+
+    config = LlamaConfig(hidden_size=hidden_size, intermediate_size=intermediate_size)
+    input_tensor = torch.randn(1, seqlen, hidden_size, dtype=torch.bfloat16).to(dev)
+
+    model1 = LlamaMLPTPSP(config, pack_weight=True).to(torch.bfloat16).to(dev)
+    output1 = model1(input_tensor)
+
+    model2 = LlamaMLPTPSP(config, pack_weight=False).to(torch.bfloat16).to(dev)
+    model2.w2.weight.data.copy_(model1.w2.weight.data)
+    blk_size = intermediate_size // _world_size
+    model2.w1.weight.data.copy_(model1.w1w3.weight.data[:blk_size, :])
+    model2.w3.weight.data.copy_(model1.w1w3.weight.data[blk_size:, :])
+    output2 = model2(input_tensor)
+
+    model3 = (
+        LlamaMLPTPSP(config, pack_weight=True, sequence_parallel=True)
+        .to(torch.bfloat16)
+        .to(dev)
+    )
+    model3.w2.weight.data.copy_(model1.w2.weight.data)
+    model3.w1w3.weight.data.copy_(model1.w1w3.weight.data)
+    output3 = model3(input_tensor)
+
+    print(output1 - output3)
+    # testing.assert_allclose(output1, output2, rtol=1e-1)
+
+
+if __name__ == "__main__":
+    test_LlamaMLPTPSP()
